4dfdfd2461780fb2ba1f61cc915a6dc3b0eaee56
TheBaldWonder <heilman.jonathan83@gmail.com>
2019-04-18 16:18:24 -0700

    Adds create method and views
    Now we can add news items.

--- sp19/application/views/news/view.php
+++ sp19/application/views/news/view.php
@@ -1,4 +1,11 @@
 <?php
 //application/views/news/view.php
+$this->load->view($this->config->item('theme') . 'header');
+
 echo '<h2>'.$news_item['title'].'</h2>';
 echo $news_item['text'];
+
+
+//application/views/news/view.php
+$this->load->view($this->config->item('theme') . 'footer');
+
