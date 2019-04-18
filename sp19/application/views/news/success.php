9fdb2958e8c22e08ca73ced7d4ff81188d0f942e
TheBaldWonder <heilman.jonathan83@gmail.com>
2019-04-18 16:52:07 -0700

    Update success.php

--- sp19/application/views/news/success.php
+++ sp19/application/views/news/success.php
@@ -1,6 +1,14 @@
 <?php
 //application/views/news/success.php
+$this->load->view($this->config->item('theme') . 'header');
 ?>
 
 <h1>Success!</h1>
 <p>Wouldn't it be awesome if i could see the new record?</p>
+
+<?php
+
+
+$this->load->view($this->config->item('theme') . 'footer');
+
+?>
