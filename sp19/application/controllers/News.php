a0d6d137a5d637c554454bea70829a4007e21e6a
TheBaldWonder <heilman.jonathan83@gmail.com>
2019-04-23 17:38:40 -0700

    Adds custom title tags

--- sp19/application/controllers/News.php
+++ sp19/application/controllers/News.php
@@ -5,22 +5,31 @@ class News extends CI_Controller {
         public function __construct()
         {
                 parent::__construct();
+                $this->config->set_item('banner','News Section');
                 $this->load->model('news_model');
                 $this->load->helper('url_helper');
         }
 
         public function index()
         {
+        $this->config->set_item('title','Seattle Sports News');
+            
+        $nav1 = $this->config->item('nav1');
+            
         $data['news'] = $this->news_model->get_news();
         $data['title'] = 'News archive';
-
-        //$this->load->view('templates/header', $data);
         $this->load->view('news/index', $data);
-        //$this->load->view('templates/footer');
         }
 
         public function view($slug = NULL)
         {
+        //slug without dashes
+        $dashless_slug = str_replace("-", " ", $slug);
+        //titlecase slug words
+        $dashless_slug = ucwords($dashless_slug);
+        //use dashless slug for title
+        $this->config->set_item('title','News Flash - ' . $dashless_slug);
+            
         $data['news_item'] = $this->news_model->get_news($slug);
 
         if (empty($data['news_item']))
@@ -37,10 +46,11 @@ class News extends CI_Controller {
     
         public function create()
     {
+        $this->config->set_item('title','Create a News Item');
         $this->load->helper('form');
         $this->load->library('form_validation');
 
-        $data['title'] = 'Create a news item';
+        $data['title'] = 'Create a News Item';
 
         $this->form_validation->set_rules('title', 'Title', 'required');
         $this->form_validation->set_rules('text', 'Text', 'required');
